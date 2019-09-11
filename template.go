3ba1ab660b6cb6cade18b726e2f516bda2b7135a
gaetan <gaetan.depin@fr.clara.net>
2019-09-11 11:55:22 +0200

    extend template support

--- template.go
+++ template.go
@@ -6,10 +6,11 @@ import (
 
 // https://www.zabbix.com/documentation/2.2/manual/api/reference/template/object
 type Template struct {
-	TemplateId  string `json:"templateid,omitempty"`
-	Host        string `json:"host"`
-	Description string `json:"description,omitempty"`
-	Name        string `json:"name,omitempty"`
+	TemplateId  string     `json:"templateid,omitempty"`
+	Host        string     `json:"host"`
+	Description string     `json:"description,omitempty"`
+	Name        string     `json:"name,omitempty"`
+	Groups      HostGroups `json:"groups"`
 }
 
 type Templates []Template
@@ -31,5 +32,48 @@ func (api *API) TemplatesGet(params Params) (res Templates, err error) {
 	}
 
 	reflector.MapsToStructs2(response.Result.([]interface{}), &res, reflector.Strconv, "json")
+	return
+}
+
+func (api *API) TemplatesCreate(templates Templates) (err error) {
+	response, err := api.CallWithError("template.create", templates)
+	if err != nil {
+		return
+	}
+
+	result := response.Result.(map[string]interface{})
+	templateids := result["templateids"].([]interface{})
+	for i, id := range templateids {
+		templates[i].TemplateId = id.(string)
+	}
+	return
+}
+
+func (api *API) TemplatesDelete(templates Templates) (err error) {
+	templatesIds := make([]string, len(templates))
+	for i, template := range templates {
+		templatesIds[i] = template.TemplateId
+	}
+
+	err = api.TemplatesDeleteByIds(templatesIds)
+	if err == nil {
+		for i := range templates {
+			templates[i].TemplateId = ""
+		}
+	}
+	return
+}
+
+func (api *API) TemplatesDeleteByIds(ids []string) (err error) {
+	response, err := api.CallWithError("template.delete", ids)
+	if err != nil {
+		return
+	}
+
+	result := response.Result.(map[string]interface{})
+	templateids := result["templateids"].([]interface{})
+	if len(ids) != len(templateids) {
+		err = &ExpectedMore{len(ids), len(templateids)}
+	}
 	return
 }
