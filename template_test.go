354c49f6a82adaeb640b1f27beb2b580680577f6
gaetan <gaetan.depin@fr.clara.net>
2019-09-11 12:19:25 +0200

    add template test

--- template_test.go
+++ template_test.go
@@ -1,12 +1,35 @@
 package zabbix_test
 
 import (
-	. "."
 	"testing"
+
+	. "."
 )
+
+func CreateTemplate(hostGroup *HostGroup, t *testing.T) *Template {
+	template := Templates{Template{
+		Host:   "template name",
+		Groups: HostGroups{*hostGroup},
+	}}
+	err := getAPI(t).TemplatesCreate(template)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &template[0]
+}
+
+func DeleteTemplate(template *Template, t *testing.T) {
+	err := getAPI(t).TemplatesDelete(Templates{*template})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestTemplates(t *testing.T) {
 	api := getAPI(t)
+
+	hostGroup := CreateHostGroup(t)
+	defer DeleteHostGroup(hostGroup, t)
 
 	templates, err := api.TemplatesGet(Params{})
 	if err != nil {
@@ -16,4 +39,11 @@ func TestTemplates(t *testing.T) {
 	if len(templates) == 0 {
 		t.Fatal("No templates were obtained")
 	}
+
+	template := CreateTemplate(hostGroup, t)
+	if template.TemplateId == "" {
+		t.Errorf("Template id is empty %#v", template)
+	}
+
+	DeleteTemplate(template, t)
 }
